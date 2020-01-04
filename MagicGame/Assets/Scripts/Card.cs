41bcc40e2dbdaea428f87b47e202355c302b9715
Zachary Lloyd <zachlloyd01@gmail.com>
2020-01-03 23:47:33 -0500

    Create Cards from JSON data!!!!!!
    Create new card prefabs from a json file :O

--- MagicGame/Assets/Scripts/Card.cs
+++ MagicGame/Assets/Scripts/Card.cs
@@ -17,4 +17,23 @@ public class Card : ScriptableObject //Scriptable Object allows Unity to create 
     public int attack;
     public int damage; //Is this the right name?
 
+    /**
+     * This part of code is for initializing object from code
+     **/
+
+    public void Init(string name, string type, string description, int attack, int damage) //Data pass-thru
+    {
+        this.name = name;
+        this.type = type;
+        this.description = description;
+        this.attack = attack;
+        this.damage = damage;
+    }
+
+    public static Card CreateInstance(string name, string type, string description, int attack, int damage) //Custom Constructor
+    {
+        var data = ScriptableObject.CreateInstance<Card>(); //an abstract var to hold the instance
+        data.Init(name, type, description, attack, damage); //initialize the new data (WOW!? SELF EXPLANATORY AMIRITE??????)
+        return data;
+    }
 }
---
+++ MagicGame/Assets/Scripts/During_Dev/Create_Cards.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+public class Create_Cards : MonoBehaviour
+{
+
+    public GameObject cardPrefab; //The prefab to create instances from
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        createobjects();
+    }
+
+    private void createobjects()
+    {
+        string json = File.ReadAllText("cardlist.json"); //The file to read the data off of
+        var jsonCards = JsonConvert.DeserializeObject<List<cardList>>(json); //deserialize the data into a readable list format
+        foreach (cardList card in jsonCards)
+        {
+            Card i = Card.CreateInstance(card.name, card.type, card.description, int.Parse(card.attack), int.Parse(card.damage)); //Use the custom constructor to create a Card
+            GameObject instance = GameObject.Instantiate(cardPrefab) as GameObject; //Get an instance of the prefab template
+            instance.name = i.name; //set the name of the instance to the card name
+            instance.GetComponent<CardDisplay>().card = i; //set the card data of the new prefab to the new card
+            PrefabUtility.SaveAsPrefabAsset(instance, "/Cards/"); //save the new card prefab to the cards directory
+        }
+    }
+}
+
+public class cardList //The class to attach json data to
+{
+    public string name;
+    public string type;
+    public string attack;
+    public string damage;
+    public string flavorText;
+    public string description;
+}
