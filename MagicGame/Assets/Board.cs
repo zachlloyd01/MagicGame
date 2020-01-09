81b5dc1ba7a57e8a0d0516b376f6b361ae369db4
Zachary Lloyd <zachlloyd01@gmail.com>
2020-01-08 18:20:59 -0800

    Board now actually creates a deck
    Made board actually spawn the deck
    Fixed error I made last commit

---
+++ MagicGame/Assets/Board.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public GameObject deck;
+    // Start is called before the first frame update
+    void Start()
+    {
+        deck = Instantiate(deck);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
