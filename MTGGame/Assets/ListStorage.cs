0d6c801d34e5107d1b8535b1c24c8c06de6820fe
Zachary Lloyd <zachlloyd01@gmail.com>
2020-03-27 22:08:24 -0400

    merge data use testing

---
+++ MTGGame/Assets/ListStorage.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.Generic;
+//using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ListStorage : MonoBehaviour
+{
+    public CardData[] cards;
+
+    private void Awake()
+    {
+        //DontDestroyOnLoad(gameObject);
+       /* var objs = AssetDatabase.LoadAllAssetsAtPath("Assets/CardData/JsonObjects");
+        foreach (TextAsset json in objs)
+        {
+            CardData card = JsonConvert.DeserializeObject<CardData>(json.ToString());
+            cards.Add(card);
+        }*/
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
