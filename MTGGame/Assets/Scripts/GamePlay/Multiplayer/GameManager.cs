dccf5ed13d4b6d3df42c32d7a7e55c3db16422bf
fried-man <andrewfriedman01@gmail.com>
2020-04-05 22:35:11 -0400

    init turn order

--- MTGGame/Assets/Scripts/GamePlay/Multiplayer/GameManager.cs
+++ MTGGame/Assets/Scripts/GamePlay/Multiplayer/GameManager.cs
@@ -4,25 +4,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+
+using System;
 
 public class GameManager : MonoBehaviourPunCallbacks
 {
 
     public GameObject pausePanel;
     public List<GameObject> Players;
+    public List<string> orderTurns;
+    public TMP_Text centerText;
+
+    int currentPlayer;
 
     private void Start()
     {
         PhotonNetwork.AutomaticallySyncScene = true;
 
+        this.gameObject.AddComponent<PhotonView>();
         List<Photon.Realtime.Player> turnOrder = new List<Photon.Realtime.Player>();
-        foreach(Photon.Realtime.Player player in PhotonNetwork.PlayerList)
+        foreach (Photon.Realtime.Player player in PhotonNetwork.PlayerList)
         {
             // turnOrder.Add(player);
             Players.Add(new GameObject());
             Players[Players.Count - 1].AddComponent<game_player>();
             Players[Players.Count - 1].name = player.NickName;
+            orderTurns.Add(player.NickName);
         }
+        if (PhotonNetwork.IsMasterClient) {
+            orderTurns.Shuffle();
+            // String.Join(",", orderTurns.ToArray()).ToString()
+            this.photonView.RPC("getOrder", RpcTarget.All, orderTurns.ToArray()); // Send to other players
+        }
+        currentPlayer = 0;
     }
 
     #region Photon Callbacks
@@ -62,6 +77,13 @@ public class GameManager : MonoBehaviourPunCallbacks
         else if(Input.GetKeyDown(KeyCode.Escape) && pausePanel.activeSelf.Equals(true)) {
             pausePanel.SetActive(false);
         }
+        if (orderTurns.Count > 0) {
+          string values = "Order:\n";
+          foreach (string x in orderTurns) {
+              values += x + "\n";
+          }
+          centerText.SetText(values);
+        }
     }
 
     #endregion
@@ -87,11 +109,17 @@ public class GameManager : MonoBehaviourPunCallbacks
         }
         Debug.Log($"PhotonNetwork: Loading Level, with {PhotonNetwork.CurrentRoom.PlayerCount}");
         PhotonNetwork.LoadLevel("Game");
-
-
     }
 
     #endregion
+
+    [PunRPC]
+    public void getOrder (string[] order) {
+      if (!PhotonNetwork.IsMasterClient) {
+        orderTurns = new List<string>();
+        orderTurns.AddRange(order);
+      }
+    }
 
     #region Game Management
 
