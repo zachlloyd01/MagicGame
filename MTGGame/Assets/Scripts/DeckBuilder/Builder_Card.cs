0c906db9f12cff1d67cdaf65be63606bb59242c6
Zachary Lloyd <zachlloyd01@gmail.com>
2020-04-10 16:27:46 -0400

    Add testing scene, deckbuilding fixes

--- MTGGame/Assets/Scripts/DeckBuilder/Builder_Card.cs
+++ MTGGame/Assets/Scripts/DeckBuilder/Builder_Card.cs
@@ -4,41 +4,64 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.Networking;
+using TMPro;
+using MtgApiManager.Lib.Service;
 using UnityEngine.UI;
 
-public class Builder_Card : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+public class Builder_Card : MonoBehaviour, IPointerClickHandler //, IPointerEnterHandler, IPointerExitHandler, 
 {
-
     public string id;
+    /*
+        public string cardName;
+        public string cardText;
+        public string[] types;
+        public float? CMC;
 
-    public void OnPointerEnter(PointerEventData eventData) //display info panel
-    {
-        
-    }
+        public GameObject infoPanel;
+        public TMP_Text cardNameTitle;
+        public TMP_Text cardTextTitle;
+        public TMP_Text cardTypesTitle;
+        public TMP_Text cardCMCTitle;
 
-    public void OnPointerExit(PointerEventData eventData) //remove info panel
-    {
-        
-    }
+
+
+        private void Start()
+        {
+            CardService service = new CardService();
+            var result = service.Find(id);
+            var values = result.Value;
+            cardName = values.Name;
+            cardText = values.Text;
+            types = values.Types;
+            CMC = values.Cmc;
+            infoPanel.SetActive(false);
+            cardNameTitle.text = cardName;
+            cardTextTitle.text = cardText;
+            string type = "";
+            foreach (string i in types)
+            {
+                type += i;
+            }
+            cardTypesTitle.text = type;
+            cardCMCTitle.text = "CMC: " + CMC.ToString();
+        }
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            infoPanel.SetActive(true);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            infoPanel.SetActive(false);
+        }*/
 
     private void Update()
     {
         GetComponent<BoxCollider2D>().size = GetComponent<Image>().sprite.rect.size;
     }
 
-    public void OnPointerDown(PointerEventData eventData)
-    {
-       
-    }
-
-    public void OnPointerUp(PointerEventData eventData)
-    {
-        
-    }
-
     public void OnPointerClick(PointerEventData eventData)
     {
-        Debug.Log(id);
         GameObject.Find("Manager").GetComponent<SaveToList>().cardName = gameObject.name;
         GameObject.Find("Manager").GetComponent<SaveToList>().id = id;
         GameObject.Find("Manager").GetComponent<SaveToList>().openPanel();
