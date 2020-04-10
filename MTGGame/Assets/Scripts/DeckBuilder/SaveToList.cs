7a6473a27aab6c3d56d34488d619537f8e4d2ba0
Zachary Lloyd <zachlloyd01@gmail.com>
2020-04-10 15:45:17 -0400

    SaveToList() cleanup
    Comment things

--- MTGGame/Assets/Scripts/DeckBuilder/SaveToList.cs
+++ MTGGame/Assets/Scripts/DeckBuilder/SaveToList.cs
@@ -1,43 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using Newtonsoft.Json;
 using System.IO;
-using Newtonsoft.Json.Linq;
-using MtgApiManager.Lib.Service;
 
 public class SaveToList : MonoBehaviour
 {
+    #region Public Variables
 
-    public TMP_InputField quantity;
-    public string cardName;
-    public string id;
+    public TMP_InputField quantity; //How many cards to have
+    public string cardName; //Card to add
+    public string id; //Get the card's ID
+    public GameObject newPanel; //Panel to open
+    public TMP_Text title; //Title of the panel
 
-    public GameObject newPanel;
-
-    public TMP_Text title;
-
-    CardService service = new CardService();
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void save()
-    {
-        int number = int.Parse(quantity.text);
- 
-        appendList(id, number);
-    }
+    #endregion
 
     public void openPanel()
     {
@@ -45,13 +22,22 @@ public class SaveToList : MonoBehaviour
         newPanel.SetActive(true);
     }
 
-    private void appendList(string ID, int number)
+    #region Private Functions
+    private void save()
     {
-        string file = GetComponent<ListChooser>().workingFile;
+        int number = int.Parse(quantity.text); //How many times to add card
+        appendList(id, number); //Add card to the WOrking File
+    }
 
-        if(new FileInfo(file).Exists)
+
+
+    private void appendList(string ID, int number) //Called from save()
+    {
+        string file = GetComponent<ListChooser>().workingFile; //Get the workingfile
+
+        if (new FileInfo(file).Exists) //If the file exists
         {
-            if(new FileInfo(file).Length > 0)
+            if (new FileInfo(file).Length > 0) //If the file already has data
             {
                 string json; //WIll be set to the full json data for each card
                 using (StreamReader sr = new StreamReader(file)) //Read the file of the full JSON file
@@ -59,50 +45,53 @@ public class SaveToList : MonoBehaviour
                     json = sr.ReadToEnd(); //Read through the entire file, set json to the string that is generated
                     sr.Close(); //Close the reader
                 }
-                DeckClass deck = JsonConvert.DeserializeObject<DeckClass>(json);
-                for (int i = 0; i < int.Parse(quantity.text); i++)
+                DeckClass deck = JsonConvert.DeserializeObject<DeckClass>(json); //Generate a new DeckClass() from the file
+                for (int i = 0; i < int.Parse(quantity.text); i++) //Amount of times to add card
                 {
-                    deck.ids.Add(ID);
+                    deck.ids.Add(ID); //Add the card to DeckClass()
                 }
-                string newData = JsonConvert.SerializeObject(deck);
-                File.WriteAllText(file, newData);
+                string newData = JsonConvert.SerializeObject(deck); //Reserialize the DeckClass() instance
+                File.WriteAllText(file, newData); //Write the json to the file
+                newPanel.SetActive(false); //TUrn off add panel
             }
 
             else
             {
-                DeckClass deck = new DeckClass();
-                deck.ids = new List<string>();
-                for (int i = 0; i < int.Parse(quantity.text); i++)
+                DeckClass deck = new DeckClass(); //Create a new DeckClass()
+                deck.ids = new List<string>(); //Empty list of IDs
+                for (int i = 0; i < int.Parse(quantity.text); i++) //Amount of times to add the card
                 {
-                    deck.ids.Add(ID);
+                    deck.ids.Add(ID); //Add the card ID to the deck
                 }
-                string newData  = JsonConvert.SerializeObject(deck);
-                using (StreamWriter sw = File.CreateText(file))
+                string newData = JsonConvert.SerializeObject(deck); //Serialize the deck
+                using (StreamWriter sw = File.CreateText(file)) //Write the text
                 {
                     sw.WriteLine(newData);
                     sw.Close();
                 }
+                newPanel.SetActive(false); //Turn off add panel
             }
-            newPanel.SetActive(false);
         }
 
         else
         {
-            DeckClass deck = new DeckClass();
-            deck.ids = new List<string>();
-            for (int i = 0; i < int.Parse(quantity.text); i++)
+            DeckClass deck = new DeckClass(); //Create a new DeckClass()
+            deck.ids = new List<string>(); //Empty list of IDs
+            for (int i = 0; i < int.Parse(quantity.text); i++) //Amount of times to add the card
             {
-                deck.ids.Add(ID);
+                deck.ids.Add(ID); //Add the card ID to the deck
             }
-            string newData = JsonConvert.SerializeObject(deck);
-            using (StreamWriter sw = File.CreateText(file))
+            string newData = JsonConvert.SerializeObject(deck); //Serialize the deck
+            using (StreamWriter sw = File.CreateText(file)) //Write the text
             {
                 sw.WriteLine(newData);
                 sw.Close();
             }
-            newPanel.SetActive(false);
+            newPanel.SetActive(false); //Turn off add panel
         }
 
-        
+
     }
+
+    #endregion
 }
